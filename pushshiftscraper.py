a6eb5debdd3c78028a0fda4bd9f6ca2dc9f391d2
David Hoppenbrouwers <david@demindiro.com>
2019-03-30 21:02:03 +0100

    Added scraper for pushshift.io

--- pushshiftscraper.py
+++ pushshiftscraper.py
@@ -1,12 +1,12 @@
 #!venv/bin/python3
 
 from config import *
-from praw import Reddit
 from time import sleep
 from os import symlink
 from os.path import isfile, join as pathjoin
 import requests
 from mimetypes import guess_extension
+import json
 
 
 
@@ -14,18 +14,70 @@ from mimetypes import guess_extension
 Globals
 """
 
-reddit = Reddit(
-    client_id     = CLIENT_ID,
-    client_secret = CLIENT_SECRET,
-    user_agent    = USER_AGENT
-)
+if isfile('id_cache.txt'):
+    with open('id_cache.txt', 'r') as f:
+        cache = set([s.rstrip() for s in f.readlines()])
+    cache_file = open('id_cache.txt', 'a')
+else:
+    cache = set()
+    cache_file = open('id_cache.txt', 'w')
+
+
+
+"""
+Classes
+"""
+
+class Submission:
+
+    def __init__(self, id, title, url, selftext, created_utc):
+        self.id          = id
+        self.title       = title
+        self.url         = url
+        self.selftext    = selftext
+        self.created_utc = created_utc
+
+    def _parse(data):
+        return Submission(data['id'], data['title'], data['url'], data['selftext'], data['created_utc'])
+
+
+class SubmissionGenerator:
+
+    def __init__(self):
+        self.before = None
+        self._next_batch()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index >= len(self._batch):
+            self._next_batch()
+        submission = self._batch[self._index]
+        self._index += 1
+        return submission
+
+    def _next_batch(self):
+        url = f'https://api.pushshift.io/reddit/submission/search?subreddit={SUBREDDIT}&sort=new&size=1024'
+        if self.before:
+            url += '&before=' + str(self.before)
+        req = requests.get(url)
+        req.raise_for_status()
+        self._batch = [Submission._parse(j) for j in json.loads(req.text)['data']]
+        self._index = 0
+        self.before = self._batch[-1].created_utc
 
 
 """
 Main
 """
 
-for submission in reddit.subreddit(SUBREDDIT).new(limit=9999999999999999):
+for submission in SubmissionGenerator():
+
+    if submission.id in cache:
+        print('Skipping', submission.id, '(cache)')
+        continue
+
     if submission.selftext != '':
         ext = '.txt'
     else:
@@ -48,6 +100,7 @@ for submission in reddit.subreddit(SUBREDDIT).new(limit=9999999999999999):
                 ext = '.jpg'
         else:
             print('Skipping ' + submission.id + ' (MIME type is ' + ctype + ')')
+            sleep(SCRAPE_SKIP_DELAY)
             continue # TODO what to do with other types of pages (e.g. articles?)
 
     path = submission.id + ext
@@ -70,10 +123,12 @@ for submission in reddit.subreddit(SUBREDDIT).new(limit=9999999999999999):
                         print('Download failed: ' + str(ex))
                         continue
         try:
-            symlink(path, pathjoin(DOWNLOADS_DIR, submission.title.replace('/', '_')))
+            symlink(path, pathjoin(DOWNLOADS_DIR, submission.title.replace('/', '_')[:256]))
         except FileExistsError as ex:
             print('Symlink for ' + submission.id + ' already exists!')
         sleep(SCRAPE_DELAY)
     else:
-        print(submission.id + ' has already been scraped')
         sleep(SCRAPE_SKIP_DELAY)
+        print(submission.id + ' has already been scraped')
+
+    print(submission.id, file=cache_file)
