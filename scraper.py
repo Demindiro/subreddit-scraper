199caada2c877740cafc55babfaba8a7ec5978fa
David Hoppenbrouwers <david@demindiro.com>
2019-03-30 18:03:53 +0100

    Reverting changes with batching
    PRAW already has batching:
    https://github.com/praw-dev/praw/blob/master/praw/models/listing/generator.py#L58
    
    The reason it doesn't work is because of Reddit's backend:
    https://www.reddit.com/r/redditdev/comments/95ghuo/submission_paging_hard_limit/

--- scraper.py
+++ scraper.py
@@ -25,84 +25,54 @@ reddit = Reddit(
 Main
 """
 
-after_id = None
-batch_num = 0
-total_dl   = 0
-total_skip = 0
+for submission in reddit.subreddit(SUBREDDIT).new(limit=9999999999999999):
+    if submission.selftext != '':
+        ext = '.txt'
+    else:
+        req = requests.head(submission.url)
+        ctype = req.headers.get('content-type', '')
+        if ctype.startswith('image/'):
+            ext = guess_extension(ctype.split(' ', 2)[0])
+            # Really? `image/*`?
+            if ext is None:
+                print('Extension couldn\'t be guessed (' + ctype + ')')
+                # I'm done, this will do
+                # I mean, come on Reddit: https://i.reddituploads.com/9cf2711854b84c0cb5f143f5456ad031?fit=max&h=1536&w=1536&s=7aa0269674cdd9c1170a1320c1997a61
+                # ^^^ You expect me to guess the extension from that?
+                # And no, I'm not going to analyze the image itself damnit
+                ext = submission.url[submission.url.rfind('.'):].replace('/', '_')
+                print('Guessing ' + ext + ' from URL (' + submission.url + ')')
+            # Goddamnit mimetypes
+            # Yes, .jpe is valid but come on, nobody uses that anymore
+            if ext == '.jpe':
+                ext = '.jpg'
+        else:
+            print('Skipping ' + submission.id + ' (MIME type is ' + ctype + ')')
+            continue # TODO what to do with other types of pages (e.g. articles?)
 
-while True:
-    print('=== BATCH ' + str(batch_num) + ' ===')
-    batch_num += 1
-    batch_dl   = 0
-    batch_skip = 0
-    for submission in reddit.subreddit(SUBREDDIT).search(
-            'subreddit:' + SUBREDDIT, # Empty queries don't work so...?
-            sort   = SORT,
-            # t3_ == type is a link == a submission, basically
-            params = { 'after': 't3_' + str(after_id) },
-            limit  = BATCH_SIZE
-        ):
+    path = submission.id + ext
+
+    if not isfile(pathjoin(DOWNLOADS_DIR, path)):
+        print('Saving ' + submission.id)
         if submission.selftext != '':
-            ext = '.txt'
+            with open(pathjoin(DOWNLOADS_DIR, path), 'w') as f:
+                f.write(submission.selftext)
         else:
-            req = requests.head(submission.url)
-            ctype = req.headers.get('content-type', '')
-            if ctype.startswith('image/'):
-                ext = guess_extension(ctype.split(' ', 2)[0])
-                # Really? `image/*`?
-                if ext is None:
-                    print('Extension couldn\'t be guessed (' + ctype + ')')
-                    # I'm done, this will do
-                    # I mean, come on Reddit: https://i.reddituploads.com/9cf2711854b84c0cb5f143f5456ad031?fit=max&h=1536&w=1536&s=7aa0269674cdd9c1170a1320c1997a61
-                    # ^^^ You expect me to guess the extension from that?
-                    # And no, I'm not going to analyze the image itself damnit
-                    ext = submission.url[submission.url.rfind('.'):].replace('/', '_')
-                    print('Guessing ' + ext + ' from URL (' + submission.url + ')')
-                # Goddamnit mimetypes
-                # Yes, .jpe is valid but come on, nobody uses that anymore
-                if ext == '.jpe':
-                    ext = '.jpg'
-            else:
-                print('Skipping ' + submission.id + ' (MIME type is ' + ctype + ')')
-                batch_skip += 1
-                continue # TODO what to do with other types of pages (e.g. articles?)
-
-        path = submission.id + ext
-
-        if not isfile(pathjoin(DOWNLOADS_DIR, path)):
-            print('Saving ' + submission.id)
-            if submission.selftext != '':
-                with open(pathjoin(DOWNLOADS_DIR, path), 'w') as f:
-                    f.write(submission.selftext)
-            else:
-                with open(pathjoin(DOWNLOADS_DIR, path), 'wb') as f:
-                    print('Downloading ' + submission.id + ' (' + submission.url + ')')
-                    with requests.get(submission.url, stream=True) as r:
-                        try:
-                            r.raise_for_status()
-                            for chunk in r.iter_content(CHUNK_SIZE):
-                                if chunk:
-                                    f.write(chunk)
-                        except Exception as ex:
-                            print('Download failed: ' + str(ex))
-            try:
-                symlink(path, pathjoin(DOWNLOADS_DIR, submission.title.replace('/', '_')))
-            except FileExistsError as ex:
-                print('Symlink for ' + submission.id + ' already exists!')
-            batch_dl += 1
-            sleep(SCRAPE_DELAY)
-        else:
-            print(submission.id + ' has already been scraped')
-            batch_skip += 1
-            sleep(SCRAPE_SKIP_DELAY)
-
-    total_dl   += batch_dl
-    total_skip += batch_skip
-    if batch_dl == 0 and batch_skip == 0:
-        break
-    after_id = submission.id
-
-
-print('Total batches:', batch_num)
-print('Total downloads:', total_dl)
-print('Total skips:', total_skip)
+            with open(pathjoin(DOWNLOADS_DIR, path), 'wb') as f:
+                print('Downloading ' + submission.id + ' (' + submission.url + ')')
+                with requests.get(submission.url, stream=True) as r:
+                    try:
+                        r.raise_for_status()
+                        for chunk in r.iter_content(CHUNK_SIZE):
+                            if chunk:
+                                f.write(chunk)
+                    except Exception as ex:
+                        print('Download failed: ' + str(ex))
+        try:
+            symlink(path, pathjoin(DOWNLOADS_DIR, submission.title.replace('/', '_')))
+        except FileExistsError as ex:
+            print('Symlink for ' + submission.id + ' already exists!')
+        sleep(SCRAPE_DELAY)
+    else:
+        print(submission.id + ' has already been scraped')
+        sleep(SCRAPE_SKIP_DELAY)
